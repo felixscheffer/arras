818eec82584f8be737f3a62ae4e50ba96e2c8b60
Felix Scheffer <mail@felix-scheffer.de>
2014-10-19 20:26:58 +0200

    added a very basic MediumEditorIT test

---
+++ arras-components-demo/src/test/java/org/github/fscheffer/arras/demo/MediumEditorIT.java
@@ -0,0 +1,53 @@
+package org.github.fscheffer.arras.demo;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class MediumEditorIT extends ArrasTestCase {
+
+    @BeforeMethod
+    void before() {
+        open("/MediumEditorDemo");
+        click(By.linkText("Reset demo"));
+        waitForPageToLoad();
+    }
+
+    @Test
+    void testNoToolbar() {
+
+        By by = By.id("noToolbar");
+
+        click(by);
+
+        sendKeys(by, "foobar");
+
+        clickSave();
+
+        assertTextPresent(by, "foobar");
+    }
+
+    @Test
+    void testDisabled() {
+
+        By by = By.id("disabled");
+
+        assertTextPresent(by, "Hello World!");
+
+        click(by);
+
+        sendKeys(by, " foobar");
+
+        clickSave();
+
+        assertTextNotPresent(by, "foobar");
+    }
+
+    private void clickSave() {
+
+        click(By.id("submit_0"));
+
+        waitForPageToLoad();
+    }
+
+}
