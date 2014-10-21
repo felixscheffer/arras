6d5fb45759da621a1122fcfac59a0658d6054ff2
Felix Scheffer <mail@felix-scheffer.de>
2014-10-21 15:22:28 +0200

    added RemoteSubmitIT

--- arras-components-demo/src/test/java/org/github/fscheffer/arras/demo/RemoteSubmitIT.java
+++ arras-components-demo/src/test/java/org/github/fscheffer/arras/demo/RemoteSubmitIT.java
@@ -1,3 +1,15 @@
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package org.github.fscheffer.arras.demo;
 
 import org.openqa.selenium.By;
@@ -23,7 +35,7 @@ public class RemoteSubmitIT extends ArrasTestCase {
         waitForPageToLoad();
 
         assertTextPresent(By.cssSelector(".alert > span"),
-            "Triggered form with visible submit button! Form content was: \"Hello World!\"");
+                          "Triggered form with visible submit button! Form content was: \"Hello World!\"");
     }
 
     @Test
@@ -36,6 +48,6 @@ public class RemoteSubmitIT extends ArrasTestCase {
         waitForPageToLoad();
 
         assertTextPresent(By.cssSelector(".alert > span"),
-                          "Triggered form with invisible submit button! Form content was: \"Hello Tapestry!\"");
+            "Triggered form with invisible submit button! Form content was: \"Hello Tapestry!\"");
     }
 }
