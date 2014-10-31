53e7debd2f6650180fd5d16e77d448a91eabf8b8
Felix Scheffer <mail@felix-scheffer.de>
2014-10-31 15:41:29 +0100

    removed hidden form field from MediumEditor

--- arras-components/src/main/java/org/github/fscheffer/arras/components/MediumEditor.java
+++ arras-components/src/main/java/org/github/fscheffer/arras/components/MediumEditor.java
@@ -16,47 +16,63 @@ import javax.inject.Inject;
 
 import org.apache.tapestry5.Asset;
 import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.MarkupWriter;
 import org.apache.tapestry5.annotations.AfterRender;
+import org.apache.tapestry5.annotations.BeginRender;
 import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Path;
-import org.apache.tapestry5.corelib.base.AbstractTextField;
+import org.apache.tapestry5.annotations.SupportsInformalParameters;
 import org.apache.tapestry5.ioc.internal.util.InternalUtils;
 import org.apache.tapestry5.json.JSONArray;
 import org.apache.tapestry5.services.javascript.JavaScriptSupport;
+import org.github.fscheffer.arras.ArrasConstants;
 import org.github.fscheffer.arras.ArrasUtils;
+import org.slf4j.Logger;
 
+// TODO: replaced with MediumEditor with Medium.js because it seems to be more sophisticated and easier to extend
+//       also supports AMD out of the box
+//       http://jakiestfu.github.io/Medium.js/docs/
 @Import(module = "arras/medium-editor", stylesheet = { "medium-editor/medium-editor.css",
 "medium-editor/themes/default.css" })
-public class MediumEditor extends AbstractTextField {
+@SupportsInformalParameters
+public class MediumEditor {
+
+    @Parameter
+    private String             value;
 
     // default value is "bold,italic,underline,anchor,header1,header2,quote"
     @Parameter(defaultPrefix = BindingConstants.LITERAL)
-    private String            buttons;
+    private String             buttons;
+
+    @Parameter
+    private boolean            disabled;
 
     @Parameter(value = "true")
-    private boolean           toolbar;
+    private boolean            toolbar;
 
     @Parameter(value = "true")
-    private boolean           fontawesome;
+    private boolean            fontawesome;
 
     @Inject
-    private JavaScriptSupport support;
+    private JavaScriptSupport  support;
 
     @Inject
     @Path("font-awesome/css/font-awesome.css")
-    private Asset             fontAwesomeCss;
+    private Asset              fontAwesomeCss;
 
-    @Override
-    protected void writeFieldTag(MarkupWriter writer, String value) {
+    @Inject
+    private ComponentResources resources;
 
-        writer.element("div", "data-container-type", "medium-editor");
+    @Inject
+    private Logger             logger;
 
-        writer.element("input", "type", "hidden", "name", getControlName());
-        writer.end();
+    @BeginRender
+    void begin(MarkupWriter writer) {
 
-        writer.element("div", "name", getControlName(), "id", getClientId());
+        writer.element("div", "data-container-type", "medium-editor", "data-context", this.resources.getCompleteId());
 
         ArrasUtils.addOption(writer, "disable-editing", this.disabled);
         ArrasUtils.addOption(writer, "buttons", splitIntoJsonArray(this.buttons));
@@ -72,8 +88,8 @@ public class MediumEditor extends AbstractTextField {
             ArrasUtils.addDataAttribute(writer, "fontawesome", false);
         }
 
-        if (InternalUtils.isNonBlank(value)) {
-            writer.writeRaw(value);
+        if (InternalUtils.isNonBlank(this.value)) {
+            writer.writeRaw(this.value);
         }
     }
 
@@ -94,7 +110,16 @@ public class MediumEditor extends AbstractTextField {
 
         this.resources.renderInformalParameters(writer);
 
-        writer.end(); // div
         writer.end(); // div[medium-editor]
+    }
+
+    @OnEvent(ArrasConstants.UPDATE_CONTENT)
+    boolean onContentSubmit(String newValue) {
+
+        this.logger.info("Saving {}", newValue);
+
+        this.value = newValue;
+
+        return true;
     }
 }
