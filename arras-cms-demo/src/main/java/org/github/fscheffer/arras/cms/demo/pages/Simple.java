aa55ab2ad7db0597d32a17765db621931ff09778
Felix Scheffer <mail@felix-scheffer.de>
2014-10-31 21:25:50 +0100

    hide the submit button when not in edit mode

--- arras-cms-demo/src/main/java/org/github/fscheffer/arras/cms/demo/pages/Simple.java
+++ arras-cms-demo/src/main/java/org/github/fscheffer/arras/cms/demo/pages/Simple.java
@@ -73,6 +73,10 @@ public class Simple {
         this.alerts.success("Changes saved!");
     }
 
+    public boolean isEditing() {
+        return this.permissionManager.hasPermissionToEdit();
+    }
+
     public String getH1() {
         return ArrasUtils.get(this.data, "h1", "<h1>A Great Headline</h1>");
     }
@@ -92,7 +96,7 @@ public class Simple {
     public String getSubtitel() {
         return ArrasUtils.get(this.data,
                               "subtitel",
-            "<p>A great way to catch your reader's attention is to tell a story. Everything you consider writing can be told as a story.</p>");
+                              "<p>A great way to catch your reader's attention is to tell a story. Everything you consider writing can be told as a story.</p>");
     }
 
     public void setSubtitel(String value) {
@@ -102,7 +106,9 @@ public class Simple {
     public String getContent() {
         return ArrasUtils.get(this.data,
                               "content",
-            "<p><b>Great stories have personality.</b> Consider telling a great story that provides personality. Writing a story with personality for potential clients will asist with making a relationship connection. This shows up in small quirks like word choices or phrases. Write from your point of view, not from someone else's experience.</p><p><b>Great stories are for everyone even when only written for just one person.</b> If you try to write with a wide general audience in mind, your story will ring false and be bland. No one will be interested. Write for one person. If it’s genuine for the one, it’s genuine for the rest.</p>");
+                              "<p>But as the junior mates were hurrying to execute the order, a pale man, with a bandaged head, arrested them—<b>Radney</b> the <i>chief mate</i>. Ever since the blow, he had lain in his berth; but that morning, hearing the tumult on the deck, he had crept out, and thus far had watched the whole scene. Such was the state of his mouth, that he could hardly speak; but mumbling something about his being willing and able to do what the captain dared not attempt, he snatched the rope and advanced to his pinioned foe.</p>"
+                                  + "<p>In his treatise on <b>\"Queen-Gold,\"</b> or Queen-pinmoney, an old King's Bench author, one William Prynne, thus discourseth: \"Ye tail is ye Queen's, that ye Queen's wardrobe may be supplied with ye whalebone.\" Now this was written at a time when the <i>black limber bone of the Greenland or Right whale</i> was largely used in ladies' bodices. But this same bone is not in the tail; it is in the head, which is a sad mistake for a sagacious lawyer like Prynne. But is the Queen a mermaid, to be presented with a tail? An allegorical meaning may lurk here.</p>"
+                                  + "<p>There are two royal fish so styled by the English law writers—the whale and the sturgeon; both royal property under certain limitations, and nominally supplying the tenth branch of the crown's ordinary revenue. I know not that any other author has hinted of the matter; but by inference it seems to me that the sturgeon must be divided in the same way as the whale, the King receiving the highly dense and elastic head peculiar to that fish, which, symbolically regarded, may possibly be humorously grounded upon some presumed congeniality. And thus there seems a reason in all things, even in law.</p>");
     }
 
     public void setContent(String value) {
