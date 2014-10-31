15603556ceb7252df6f508e9c1c960212191cd67
Felix Scheffer <mail@felix-scheffer.de>
2014-10-31 14:57:40 +0100

    content-container does now handle json responses properly and moved submit handlers to the actual component file

--- arras-cms/src/main/resources/META-INF/modules/content-container.js
+++ arras-cms/src/main/resources/META-INF/modules/content-container.js
@@ -1,135 +1,112 @@
 (function() {
-	
-	define(["t5/core/dom", "event-hub"], function(dom, EventHub) {
 
-		var containerSel = "[data-container-type=content-container]";
-		var blockSel = "[data-component-type=content-block]";
-		var parentSel = blockSel + "," + containerSel;
-		
-		var SUBMIT_EVENT = "arras:submit";
-	
-		var Walker = function(element, selectors) {
-			var selector = selectors.join(",");
-			this.elements = element.find(selector);
-			this.current = null;
-		}
-		
-		Walker.prototype.forEach = function(callback) {
-			
-			var parent = this.current;
-			
-			var lastElement = null;
-			
-			while(i<this.elements.length) {
-				
-				var element = this.elements[i];
-				
-				if(parent != null && !parent.element.contains(element.element)) {
-					// this is no longer a subelement
-					break;
-				}
-				
-				i++;
-				
-				if(lastElement == null || !lastElement.element.contains(element.element)) {
-					this.current = element;
-					lastElement = element;
-					console.log("processing: "+element.attr("data-context"));
-					callback(element);
-				}
-				
-				//else:
-				//  skip subelements - they should have been handled by the callback
-				
+	define([ "t5/core/dom", "t5/core/pageinit", "t5/core/events", "./content-events" ], function(
+			dom, pi, t5events, events) {
+
+		var CONTAINER_SELECTOR = "[data-container-type=content-container]";
+		var BLOCK_SELECTOR = "[data-component-type=content-block]";
+		var PARENT_SELECTOR = BLOCK_SELECTOR + "," + CONTAINER_SELECTOR;
+
+		dom.onDocument("click", "[data-container-type=content-submit]",
+				function() {
+
+					var container = this.closest(CONTAINER_SELECTOR);
+
+					var url = container.attr("data-url");
+					var data = {};
+
+					container.trigger(events.submit, data);
+
+					dom.ajaxRequest(url, {
+						data : {
+							content : JSON.stringify(data)
+						},
+						success : function(response) {
+							if (response.json) {
+								
+								pi.handlePartialPageRenderResponse(response);
+								if(response.json.content) {								
+									container.update(response.json.content);
+									container.trigger(t5events.zone.didUpdate);
+								}
+							}
+						}
+					});
+				});
+
+		dom.scanner(BLOCK_SELECTOR, function(block) {
+
+			var container = block.findParent(PARENT_SELECTOR);
+
+			if (container === null) {
+				return;
 			}
-		}
-		
-		
-		dom.onDocument("click", "[data-container-type=content-submit]", function() {
-			submit(this.closest(containerSel));
-		});
-		
-		function submit(container) {
-			
-			var url = container.attr("data-url");
-			var data = {};
-			var selectors = ["[data-component-type=content-block]","[data-container-type=medium-editor]", "[data-component-type=content-image]"];
-			
-			var walker = new Walker(container, selectors);
-			walker.forEach(function(element) {
-				element.trigger("arras:submit", { data:data, walker:walker });
+
+			var off = dom.on(container.element, events.submit, submit);
+
+			block.on(events.syncHandler, function() {
+
+				if (off) {
+					off();
+				}
+
+				off = dom.on(container.element, events.submit, submit);
+
+				return false;
 			})
-			
-			dom.ajaxRequest(url, {
-				data: { content: JSON.stringify(data) }
-			});	
-		}
-		
-		dom.scanner(blockSel, function(block) {
-			
-			var container = block.findParent(parentSel);
-			
-			if(container === null) {
-				return
-			}
-			
-			
-			block.on(SUBMIT_EVENT, function(event, eventContext) {
 
-				var walker = eventContext.walker;
-				var data = eventContext.data;
+			function submit(event, data) {
+
 				var subdata = {}
 				var context = block.attr("data-context")
-				
-				if(data[context] === undefined) {
+
+				if (data[context] === undefined) {
 					data[context] = []
 				}
-				
+
 				console.log("block submit: " + context);
-				
+
 				data[context].push(subdata);
 
-				walker.forEach(function(child) {	
-					child.trigger(SUBMIT_EVENT, {data:subdata, walker:walker});
-				})
-				
-				return false;
-			})
-		})
-		
-		
-		// TODO: move to medium-editor.js
-		dom.scanner("[data-container-type=medium-editor]", function(editor) {
-			
-			editor.on(SUBMIT_EVENT, function(event, eventContext) {
-				
-				console.log("medium-editor: " + editor.attr("data-context"));
-				
-				var data = eventContext.data;
-				var context = editor.attr("data-context");
-				var content = editor.element.innerHTML.trim();
-				
-				data[context] = content;
-				
-				return false;
-			})
-		})
-		
-		dom.scanner("[data-component-type=content-image]", function(image) {
-			
-			image.on(SUBMIT_EVENT, function(event, eventContext) {
-				
-				console.log("image: " + image.attr("data-context"));
-				
-				var data = eventContext.data;
-				var context = image.attr("data-context");
-				
-				// "image" is the div wrapping the actual img tag
-				data[context] = image.findFirst("> img").attr("src");
-				
-				return false;
+				block.trigger(events.submit, subdata);
+			}
+
+			block.on(events.submit, function(event) {
+
+				// dont bubble up the events we trigger or we end up in an
+				// infinite loop when the
+				// event reaches the container
+				// block -> container -> block
+				event.nativeEvent.stopPropagation();
+			});
+
+			block.on(events.removeHandler, function(event) {
+
+				if (off) {
+					off();
+					off = null;
+				}
+
+				event.nativeEvent.stopPropagation();
 			});
 		})
-		
+
+		dom.scanner("[data-sortable]", function(sortable) {
+
+			var container = sortable.closest(CONTAINER_SELECTOR);
+
+			if (!container) {
+				// not inside of a container, so we dont care
+				return;
+			}
+
+			sortable.on("sortupdate", function() {
+
+				this.find(BLOCK_SELECTOR).forEach(function(element) {
+					// inform all blocks that the order may have changed
+					element.trigger(events.syncHandler);
+				})
+			})
+		});
 	})
 }).call(this);
