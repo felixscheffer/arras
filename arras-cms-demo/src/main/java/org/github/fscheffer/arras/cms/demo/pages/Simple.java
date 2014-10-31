35cf5b2be4d3442c7f8123beaaff4755a572dc62
Felix Scheffer <mail@felix-scheffer.de>
2014-10-31 14:48:47 +0100

    adopt changes to Simple page

--- arras-cms-demo/src/main/java/org/github/fscheffer/arras/cms/demo/pages/Simple.java
+++ arras-cms-demo/src/main/java/org/github/fscheffer/arras/cms/demo/pages/Simple.java
@@ -12,63 +12,100 @@
 
 package org.github.fscheffer.arras.cms.demo.pages;
 
-import java.util.Locale;
-
 import javax.inject.Inject;
 
-import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.EventConstants;
 import org.apache.tapestry5.alerts.AlertManager;
+import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.OnEvent;
 import org.apache.tapestry5.jpa.annotations.CommitAfter;
-import org.github.fscheffer.arras.cms.entities.PageContent;
-import org.github.fscheffer.arras.cms.entities.PageContentId;
+import org.apache.tapestry5.json.JSONArray;
+import org.apache.tapestry5.json.JSONObject;
+import org.github.fscheffer.arras.ArrasUtils;
+import org.github.fscheffer.arras.cms.ArrasCmsConstants;
+import org.github.fscheffer.arras.cms.components.ContentContainer;
 import org.github.fscheffer.arras.cms.services.PageContentDao;
+import org.github.fscheffer.arras.cms.services.PermissionManager;
+import org.slf4j.Logger;
 
 public class Simple {
 
     @Inject
-    private AlertManager       alertManager;
+    private Logger            logger;
 
     @Inject
-    private Locale             locale;
+    private PageContentDao    contentDao;
 
     @Inject
-    private ComponentResources resources;
+    private PermissionManager permissionManager;
 
     @Inject
-    private PageContentDao     contentDao;
+    private AlertManager      alerts;
+
+    private JSONObject        data;
+
+    @OnEvent(EventConstants.ACTIVATE)
+    void onActivate() {
+
+        JSONArray array = this.contentDao.getContent("toplevel");
+
+        this.data = array.length() == 0 ? new JSONObject() : array.getJSONObject(0);
+    }
+
+    @InjectComponent
+    private ContentContainer container;
 
     @CommitAfter
-    @OnEvent(value = EventConstants.SUCCESS, component = "container")
+    @OnEvent(ArrasCmsConstants.SUBMIT_CONTENT)
     void onSucess() {
-        this.alertManager.success("Changes saved!");
+
+        if (!this.permissionManager.hasPermissionToEdit()) {
+
+            this.logger.info("User has no permission to edit page '{}'!", Simple.class.getName());
+
+            this.alerts.error("User has no permission to edit page '" + Simple.class.getName() + "'!");
+        }
+
+        this.contentDao.save("toplevel", new JSONArray(this.data));
+
+        this.logger.info("Changes saved using ajax!");
+
+        this.alerts.success("Changes saved!");
     }
 
-    public PageContent getH1() {
-        return loadContent("h1", "<h1>A Great Headline</h1>");
+    public String getH1() {
+        return ArrasUtils.get(this.data, "h1", "<h1>A Great Headline</h1>");
     }
 
-    public PageContent getH2() {
-        return loadContent("h2", "<h2>A good subtitle</h2>");
+    public void setH1(String value) {
+        this.data.put("h1", value);
     }
 
-    public PageContent getSubtitel() {
-        return loadContent("subtitel",
-                           "<p>A great way to catch your reader's attention is to tell a story. Everything you consider writing can be told as a story.</p>");
+    public String getH2() {
+        return ArrasUtils.get(this.data, "h2", "<h2>A good subtitle</h2>");
     }
 
-    public PageContent getContent() {
-        return loadContent("content",
-                           "<p><b>Great stories have personality.</b> Consider telling a great story that provides personality. Writing a story with personality for potential clients will asist with making a relationship connection. This shows up in small quirks like word choices or phrases. Write from your point of view, not from someone else's experience.</p><p><b>Great stories are for everyone even when only written for just one person.</b> If you try to write with a wide general audience in mind, your story will ring false and be bland. No one will be interested. Write for one person. If it’s genuine for the one, it’s genuine for the rest.</p>");
+    public void setH2(String value) {
+        this.data.put("h2", value);
     }
 
-    private PageContent loadContent(String contentId, String defaultValue) {
+    public String getSubtitel() {
+        return ArrasUtils.get(this.data,
+                              "subtitel",
+            "<p>A great way to catch your reader's attention is to tell a story. Everything you consider writing can be told as a story.</p>");
+    }
 
-        String pageName = this.resources.getPageName();
+    public void setSubtitel(String value) {
+        this.data.put("subtitel", value);
+    }
 
-        PageContentId id = new PageContentId(pageName, this.locale, contentId, 0);
+    public String getContent() {
+        return ArrasUtils.get(this.data,
+                              "content",
+            "<p><b>Great stories have personality.</b> Consider telling a great story that provides personality. Writing a story with personality for potential clients will asist with making a relationship connection. This shows up in small quirks like word choices or phrases. Write from your point of view, not from someone else's experience.</p><p><b>Great stories are for everyone even when only written for just one person.</b> If you try to write with a wide general audience in mind, your story will ring false and be bland. No one will be interested. Write for one person. If it’s genuine for the one, it’s genuine for the rest.</p>");
+    }
 
-        return this.contentDao.getContent(id, defaultValue);
+    public void setContent(String value) {
+        this.data.put("content", value);
     }
 }
