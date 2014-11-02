fba6dbb2586e043f220368e6a6229d96a63cb8fb
Felix Scheffer <mail@felix-scheffer.de>
2014-11-02 15:50:38 +0100

    refactord DefaultContentBlocks so it's now using the new @Content annotation

--- arras-cms/src/main/java/org/github/fscheffer/arras/cms/pages/DefaultContentBlocks.java
+++ arras-cms/src/main/java/org/github/fscheffer/arras/cms/pages/DefaultContentBlocks.java
@@ -17,8 +17,10 @@ import javax.inject.Inject;
 import org.apache.tapestry5.Asset;
 import org.apache.tapestry5.annotations.Environmental;
 import org.apache.tapestry5.annotations.Path;
-import org.github.fscheffer.arras.ArrasUtils;
+import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.json.JSONObject;
 import org.github.fscheffer.arras.cms.BlockContext;
+import org.github.fscheffer.arras.cms.Content;
 
 public class DefaultContentBlocks {
 
@@ -29,70 +31,35 @@ public class DefaultContentBlocks {
     @Path("man_point-arena-stornetta.jpg")
     private Asset        defaultImage;
 
-    private String get(String property, String defaultValue) {
-        return ArrasUtils.get(this.context.data, property, defaultValue);
-    }
+    @Content(value = "literal:<h1>A Great Headline</h1>")
+    @Property
+    private String       h1;
 
-    private void set(String key, String value) {
-        this.context.data.put(key, value);
-    }
+    @Content(value = "literal:<h2>A good subtitle</h2>")
+    @Property
+    private String       h2;
 
-    public String getH1() {
-        return get("h1", "<h1>A Great Headline</h1>");
-    }
+    @Content(value = "literal:<h3>Awesome feature</h3>")
+    @Property
+    private String       h3;
 
-    public String getH2() {
-        return get("h2", "<h2>A good subtitle</h2>");
-    }
+    @Content(contentId = "content", value = "literal:<p><b>Great stories have personality.</b> Consider telling a great story that provides personality. Writing a story with personality for potential clients will asist with making a relationship connection. This shows up in small quirks like word choices or phrases. Write from your point of view, not from someone else's experience.</p><p><b>Great stories are for everyone even when only written for just one person.</b> If you try to write with a wide general audience in mind, your story will ring false and be bland. No one will be interested. Write for one person. If it’s genuine for the one, it’s genuine for the rest.</p>")
+    @Property
+    private String       articleContent;
 
-    public String getH3() {
-        return get("h3", "<h3>Awesome feature</h3>");
-    }
+    @Content(contentId = "content", value = "literal:<p><b>Watch \"Jeopardy!\"</b>, Alex Trebek's fun TV quiz game. Woven silk pyjamas exchanged for blue quartz. Brawny gods just flocked up to quiz and vex him. Adjusting quiver and bow, Zompyc killed the fox. My faxed joke won a pager in the cable TV quiz show. <i>Amazingly</i> few discotheques provide jukeboxes. </p>")
+    @Property
+    private String       featureContent;
 
-    public String getArticleContent() {
-        return get("content",
-            "<p><b>Great stories have personality.</b> Consider telling a great story that provides personality. Writing a story with personality for potential clients will asist with making a relationship connection. This shows up in small quirks like word choices or phrases. Write from your point of view, not from someone else's experience.</p><p><b>Great stories are for everyone even when only written for just one person.</b> If you try to write with a wide general audience in mind, your story will ring false and be bland. No one will be interested. Write for one person. If it’s genuine for the one, it’s genuine for the rest.</p>");
-    }
+    @Content(contentId = "content", value = "literal:<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>")
+    @Property
+    private String       teaserContent;
 
-    public String getFeatureContent() {
-        return get("content",
-            "<p><b>Watch \"Jeopardy!\"</b>, Alex Trebek's fun TV quiz game. Woven silk pyjamas exchanged for blue quartz. Brawny gods just flocked up to quiz and vex him. Adjusting quiver and bow, Zompyc killed the fox. My faxed joke won a pager in the cable TV quiz show. <i>Amazingly</i> few discotheques provide jukeboxes. </p>");
-    }
+    @Content(value = "asset:man_point-arena-stornetta.jpg")
+    @Property
+    private String       image;
 
-    public void setH1(String value) {
-        set("h1", value);
-    }
-
-    public void setH2(String value) {
-        set("h2", value);
-    }
-
-    public void setH3(String value) {
-        set("h3", value);
-    }
-
-    public void setArticleContent(String value) {
-        set("content", value);
-    }
-
-    public void setFeatureContent(String value) {
-        set("content", value);
-    }
-
-    public String getTeaserContent() {
-        return get("teaserContent",
-            "<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>");
-    }
-
-    public void setTeaserContent(String value) {
-        set("teaserContent", value);
-    }
-
-    public String getImage() {
-        return get("image", this.defaultImage.toClientURL());
-    }
-
-    public void setImage(String value) {
-        set("image", value);
+    public JSONObject getData() {
+        return this.context.data;
     }
 }
