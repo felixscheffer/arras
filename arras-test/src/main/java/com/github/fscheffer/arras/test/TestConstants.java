9f14bc416077d726068ba101144fe460db321415
Felix Scheffer <mail@felix-scheffer.de>
2014-12-03 15:15:00 +0100

    renamed TestingConstants to TestConstants

---
+++ arras-test/src/main/java/com/github/fscheffer/arras/test/TestConstants.java
@@ -0,0 +1,15 @@
+package com.github.fscheffer.arras.test;
+
+public class TestConstants {
+
+    public static final String BASE_URL   = "test.baseUrl";
+
+    public static final String REMOTE_URL = "test.remote-url";
+
+    public static final String BROWSER    = "test.browser";
+
+    public static final String VERSION    = "test.browserVersion";
+
+    public static final String PLATFORM   = "test.platform";
+
+}
