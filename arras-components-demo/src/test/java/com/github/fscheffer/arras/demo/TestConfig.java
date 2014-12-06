4f52ea61a4a3363444c828d6a648822bc3124c02
Felix Scheffer <mail@felix-scheffer.de>
2014-12-06 14:37:42 +0100

    changed chromedriver version to 2.11 for arras-components

--- arras-components-demo/src/test/java/com/github/fscheffer/arras/demo/TestConfig.java
+++ arras-components-demo/src/test/java/com/github/fscheffer/arras/demo/TestConfig.java
@@ -1,5 +1,7 @@
 package com.github.fscheffer.arras.demo;
 
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import com.github.fscheffer.arras.test.ArrasTestUtils;
@@ -10,11 +12,15 @@ import com.github.fscheffer.arras.test.TestContextFactory;
 public class TestConfig implements TestContextFactory {
 
     @Override
-    public TestContext build(DesiredCapabilities original) {
+    public TestContext build(Capabilities original) {
 
         DesiredCapabilities capabilities = new DesiredCapabilities(original);
 
         capabilities.setCapability("name", "arras-components");
+
+        if (BrowserType.CHROME.equals(capabilities.getBrowserName())) {
+            capabilities.setCapability("chromedriver-version", "2.11");
+        }
 
         ArrasTestUtils.setCapabilityFromConfiguration(capabilities, "tunnel-identifier", "TRAVIS_JOB_NUMBER");
         ArrasTestUtils.setCapabilityFromConfiguration(capabilities, "build", "TRAVIS_BUILD_NUMBER");
