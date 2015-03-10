03fc3124bfba0896d97a6e870f0df541bc8eb6cb
Felix Scheffer <mail@felix-scheffer.de>
2015-03-10 15:28:33 +0100

    placeholder attribute support

--- arras-components/src/main/java/com/github/fscheffer/arras/components/Select2.java
+++ arras-components/src/main/java/com/github/fscheffer/arras/components/Select2.java
@@ -1,8 +1,10 @@
 package com.github.fscheffer.arras.components;
 
+import org.apache.tapestry5.BindingConstants;
 import org.apache.tapestry5.MarkupWriter;
 import org.apache.tapestry5.annotations.AfterRender;
 import org.apache.tapestry5.annotations.Import;
+import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.corelib.components.Select;
 
 import com.github.fscheffer.arras.ArrasConstants;
@@ -10,8 +12,15 @@ import com.github.fscheffer.arras.ArrasConstants;
 @Import(module = "arras/select2", stylesheet = ArrasConstants.SELECT2_CSS_PATH_VALUE)
 public class Select2 extends Select {
 
+    @Parameter(defaultPrefix = BindingConstants.LITERAL)
+    private String placeholder;
+
     @AfterRender
     void afterRender2(MarkupWriter writer) {
         writer.attributes("data-component-type", "select2");
+
+        if (this.placeholder != null) {
+            writer.attributes("data-placeholder", this.placeholder);
+        }
     }
 }
