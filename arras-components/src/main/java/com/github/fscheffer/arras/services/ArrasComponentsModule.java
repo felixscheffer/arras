79388244f69385e5cfc7c56dfb25efe7adff8394
Felix Scheffer <mail@felix-scheffer.de>
2015-03-13 23:27:47 +0100

    added JavascriptStack to reduce number of javascript files

--- arras-components/src/main/java/com/github/fscheffer/arras/services/ArrasComponentsModule.java
+++ arras-components/src/main/java/com/github/fscheffer/arras/services/ArrasComponentsModule.java
@@ -17,6 +17,7 @@ import java.util.Collection;
 import org.apache.tapestry5.annotations.Path;
 import org.apache.tapestry5.ioc.Configuration;
 import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.ioc.OrderedConfiguration;
 import org.apache.tapestry5.ioc.Resource;
 import org.apache.tapestry5.ioc.ServiceBinder;
 import org.apache.tapestry5.ioc.annotations.Contribute;
@@ -24,9 +25,14 @@ import org.apache.tapestry5.ioc.services.Coercion;
 import org.apache.tapestry5.ioc.services.CoercionTuple;
 import org.apache.tapestry5.services.BindingFactory;
 import org.apache.tapestry5.services.LibraryMapping;
+import org.apache.tapestry5.services.javascript.ExtensibleJavaScriptStack;
 import org.apache.tapestry5.services.javascript.JavaScriptModuleConfiguration;
+import org.apache.tapestry5.services.javascript.JavaScriptStack;
+import org.apache.tapestry5.services.javascript.JavaScriptStackSource;
 import org.apache.tapestry5.services.javascript.ModuleManager;
+import org.apache.tapestry5.services.javascript.StackExtension;
 
+import com.github.fscheffer.arras.Arras;
 import com.github.fscheffer.arras.ArrasConstants;
 import com.github.fscheffer.arras.CollectionFilteringDataSource;
 import com.github.fscheffer.arras.FilteringDataSource;
@@ -39,6 +45,14 @@ public class ArrasComponentsModule {
 
     public static void bind(ServiceBinder binder) {
         binder.bind(SubmissionProcessor.class, SubmissionProcessorImpl.class);
+        binder.bind(JavaScriptStack.class, ExtensibleJavaScriptStack.class).withMarker(Arras.class)
+              .withId("ArrasJavaScriptStack");
+    }
+
+    @Contribute(JavaScriptStackSource.class)
+    public static void provideBuiltinJavaScriptStacks(MappedConfiguration<String, JavaScriptStack> conf,
+                                                      @Arras JavaScriptStack stack) {
+        conf.add("arras", stack);
     }
 
     public static void contributeFactoryDefaults(MappedConfiguration<String, String> conf) {
@@ -46,6 +60,18 @@ public class ArrasComponentsModule {
         conf.add(ArrasConstants.LIGHTBOX_CSS_PATH, "colorbox/example1/colorbox.css");
         conf.add(ArrasConstants.PLAYER_CSS_PATH, "video-js/video-js.css");
         conf.add(ArrasConstants.SELECT2_CSS_PATH, "select2.css");
+    }
+
+    @Contribute(JavaScriptStack.class)
+    @Arras
+    public static void setupJavaScriptStack(OrderedConfiguration<StackExtension> conf) {
+
+        for (String module : new String[] { "datatables", "dropdown", "events", "lightbox", "loading-overlay",
+            "medium-editor", "player", "remotesubmit", "select2", "sortable-jquery", "tabgroup", "transition" }) {
+
+            conf.add("arras/" + module, StackExtension.module("arras/" + module));
+        }
+
     }
 
     @Contribute(ModuleManager.class)
@@ -66,12 +92,12 @@ public class ArrasComponentsModule {
         conf.add(CoercionTuple.create(Collection.class, FilteringDataSource.class,
                                       new Coercion<Collection, FilteringDataSource>() {
 
-            @Override
-            public FilteringDataSource coerce(Collection input) {
+                                          @Override
+                                          public FilteringDataSource coerce(Collection input) {
 
-                return new CollectionFilteringDataSource(input);
-            }
-        }));
+                                              return new CollectionFilteringDataSource(input);
+                                          }
+                                      }));
     }
 
     public static void contributeContentTypeAnalyzer(MappedConfiguration<String, String> conf) {
