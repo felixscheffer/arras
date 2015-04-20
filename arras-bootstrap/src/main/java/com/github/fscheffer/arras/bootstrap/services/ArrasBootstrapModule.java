7a5ac21495bc3847494d42a3d5e96073f29fa0af
Felix Scheffer <mail@felix-scheffer.de>
2015-04-20 19:35:53 +0200

    removed code that was not supposed to be in the module file

--- arras-bootstrap/src/main/java/com/github/fscheffer/arras/bootstrap/services/ArrasBootstrapModule.java
+++ arras-bootstrap/src/main/java/com/github/fscheffer/arras/bootstrap/services/ArrasBootstrapModule.java
@@ -1,17 +1,6 @@
 package com.github.fscheffer.arras.bootstrap.services;
 
-import org.apache.tapestry5.ioc.OrderedConfiguration;
-import org.apache.tapestry5.ioc.annotations.Contribute;
-import org.apache.tapestry5.services.Core;
-import org.apache.tapestry5.services.javascript.JavaScriptStack;
-import org.apache.tapestry5.services.javascript.StackExtension;
 
 public class ArrasBootstrapModule {
 
-    @Contribute(JavaScriptStack.class)
-    @Core
-    public static void overrideBootstrapCSS(OrderedConfiguration<StackExtension> conf) {
-        conf.override("bootstrap.css", StackExtension.stylesheet("META-INF/assets/sharelinks.less"),
-                      "before:tapestry.css");
-    }
 }
