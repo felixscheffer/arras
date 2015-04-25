c9c795466fe28c73a63e467cde626b25bac7ddec
Felix Scheffer <mail@felix-scheffer.de>
2015-04-25 17:45:34 +0200

    added support for minimumInputLength and support for informal parameters

--- arras-components/src/main/java/com/github/fscheffer/arras/components/Select2.java
+++ arras-components/src/main/java/com/github/fscheffer/arras/components/Select2.java
@@ -1,26 +1,40 @@
 package com.github.fscheffer.arras.components;
 
+import javax.inject.Inject;
+
 import org.apache.tapestry5.BindingConstants;
+import org.apache.tapestry5.ComponentResources;
 import org.apache.tapestry5.MarkupWriter;
 import org.apache.tapestry5.annotations.AfterRender;
 import org.apache.tapestry5.annotations.Import;
 import org.apache.tapestry5.annotations.Parameter;
+import org.apache.tapestry5.annotations.SupportsInformalParameters;
 import org.apache.tapestry5.corelib.components.Select;
 
 import com.github.fscheffer.arras.ArrasConstants;
+import com.github.fscheffer.arras.ArrasUtils;
 
 @Import(module = "arras/select2", stylesheet = ArrasConstants.SELECT2_CSS_PATH_VALUE)
+@SupportsInformalParameters
 public class Select2 extends Select {
 
     @Parameter(defaultPrefix = BindingConstants.LITERAL)
-    private String placeholder;
+    private String             placeholder;
+
+    @Parameter(defaultPrefix = BindingConstants.LITERAL)
+    private Integer            minimumInputLength;
+
+    @Inject
+    private ComponentResources resources;
 
     @AfterRender
     void afterRender2(MarkupWriter writer) {
+
         writer.attributes("data-component-type", "select2");
 
-        if (this.placeholder != null) {
-            writer.attributes("data-placeholder", this.placeholder);
-        }
+        ArrasUtils.addNonNullOption(writer, "placeholder", this.placeholder);
+        ArrasUtils.addNonNullOption(writer, "minimum-input-length", this.minimumInputLength);
+
+        this.resources.renderInformalParameters(writer);
     }
 }
